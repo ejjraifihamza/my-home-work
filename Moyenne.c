f3a1c2724e1694ac2a215e3160732a5f95ff0513
hamza ejjraifi <ejjraifihamza@gmail.com>
2021-01-12 17:46:57 +0100

    first one

---
+++ Moyenne.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int nbr,somme;
+	float i = 0;
+	float moyenne;
+	while(nbr != -1){
+		printf("ecrire le nombre");
+		scanf("%d",&nbr);
+		somme =somme + nbr;
+		i++;
+	}
+	if(nbr == -1){
+		moyenne = (somme+1) /(i-1);
+		printf("la moyenne des nombres entrées est %f",moyenne);
+	}
+}
